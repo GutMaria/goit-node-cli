d76b21a8bda9aa497877ab5b8d527acff6efe653
GutMaria <gootmaria@gmail.com>
2024-03-07 15:10:34 +0200

    work with FS

---
+++ contacts.js
@@ -0,0 +1,44 @@
+import fs from "fs/promises";
+import path from "path";
+import { fileURLToPath } from "url";
+import { nanoid } from "nanoid";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const contactsPath = path.join(__dirname, "db", "contacts.json");
+
+export async function listContacts() {
+  const data = await fs.readFile(contactsPath);
+  return JSON.parse(data);
+}
+
+export async function getContactById(contactId) {
+  const allContacts = await listContacts();
+  const contactById = allContacts.find((item) => item.id === contactId);
+  return contactById || null;
+}
+
+export async function removeContact(contactId) {
+  const allContacts = await listContacts();
+  const index = allContacts.findIndex((item) => item.id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  const deleteContact = allContacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return deleteContact;
+}
+
+export async function addContact(name, email, phone) {
+  const allContacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  allContacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(allContacts, null, 2));
+  return newContact;
+}
